6c4518234adb35f833bd1d374184bbbbc23ed000
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-27 19:10:41 +0800

    feat: finalize MemoryCard grid

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,22 @@ import './Main.css';
 import MemoryCard from './ui/MemoryCard';
 
 const Main = () => {
+  const imgSrc =
+    'https://i.scdn.co/image/ab67616d0000b273385baa130749f6a64aef2e39';
   return (
     <main>
-      <MemoryCard />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
+      <MemoryCard imgSrc={imgSrc} />
     </main>
   );
 };
--- src/components/ui/MemoryCard.jsx
+++ src/components/ui/MemoryCard.jsx
@@ -1,8 +1,7 @@
 import './MemoryCard.css';
 
-const MemoryCard = () => {
-  const imgSrc =
-    'https://i.scdn.co/image/ab67616d0000b273385baa130749f6a64aef2e39';
+const MemoryCard = (props) => {
+  const imgSrc = props.imgSrc;
   return (
     <div className="MemoryCard">
       <img className="MemoryCard__album-art" alt="Album Art" src={imgSrc}></img>
