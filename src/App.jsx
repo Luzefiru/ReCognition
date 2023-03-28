e57942ae086bf541eef80b1b07a96cc07231b948
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-28 09:43:20 +0800

    feat: make new branch for unsplash API ver

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import './App.css';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
 import Main from './components/Main';
+import { useState } from 'react';
 
 function App() {
+  const [selectedCards, setSelectedCards] = useState([]);
+
+  const handleMemoryCardClick = (src) => {
+    console.log('hi');
+    setSelectedCards([...selectedCards, src]);
+    console.log(selectedCards);
+  };
+
   return (
     <>
       <Header />
-      <Main />
+      <Main handleMemoryCardClick={handleMemoryCardClick} />
       <Footer />
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,9 @@ import './Main.css';
 import MemoryCard from './ui/MemoryCard';
 import { useState, useEffect } from 'react';
 
-const Main = () => {
+const Main = (props) => {
   const [memoryCards, setMemoryCards] = useState([]);
+
   useEffect(() => {
     const queryTerm = 'neon';
     const imgCount = 12;
@@ -15,20 +16,33 @@ const Main = () => {
     )
       .then((response) => response.json())
       .then((data) => {
-        setMemoryCards(initializeMemoryCards(data));
+        setMemoryCards(
+          initializeMemoryCards(data, props.handleMemoryCardClick)
+        );
       });
-  }, []);
+  }, [props.handleMemoryCardClick]);
 
   return <main>{memoryCards}</main>;
 };
 
 export default Main;
 
-const initializeMemoryCards = (data) => {
+const initializeMemoryCards = (data, handleClickFn) => {
   const memoryCards = [];
   let index = 0;
   for (let i in data) {
-    memoryCards.push(<MemoryCard key={index++} imgSrc={data[i].urls.small} />);
+    memoryCards.push(
+      <MemoryCard
+        key={index++}
+        handleMemoryCardClick={() => {
+          handleClickFn(data[i].urls.small);
+        }}
+        // imgSrc={data[i].urls.small}
+        imgSrc={
+          'https://lastfm.freetls.fastly.net/i/u/500x500/df74377533dd4cdad5da59bc675d0156.jpg'
+        }
+      />
+    );
   }
   return memoryCards;
 };
