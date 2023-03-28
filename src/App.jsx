290f2587310ea4761398141b195612ebe5a3c22c
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-28 10:30:19 +0800

    feat: add randomization & score

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,12 @@ import './App.css';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
 import Main from './components/Main';
-import { useState } from 'react';
 
 function App() {
-  const [selectedCards, setSelectedCards] = useState([]);
-
-  const handleMemoryCardClick = (src) => {
-    console.log('hi');
-    setSelectedCards([...selectedCards, src]);
-    console.log(selectedCards);
-  };
-
   return (
     <>
       <Header />
-      <Main handleMemoryCardClick={handleMemoryCardClick} />
+      <Main />
       <Footer />
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,25 +2,43 @@ import './Main.css';
 import MemoryCard from './ui/MemoryCard';
 import { useState, useEffect } from 'react';
 
-const Main = (props) => {
+const Main = () => {
   const [memoryCards, setMemoryCards] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
+    const handleMemoryCardClick = (src) => {
+      const imageID = src.split('photo-')[1].split('-')[0];
+
+      console.log(selectedCards, imageID, selectedCards.indexOf(imageID));
+
+      if (selectedCards.indexOf(imageID) !== -1) {
+        alert('You picked a duplicate, score & selectedCards array resets!');
+        setScore(0);
+        setSelectedCards([]);
+      } else {
+        console.log('Not a duplicate!');
+        setSelectedCards([...selectedCards, imageID]);
+        setScore((prevScore) => prevScore + 1);
+      }
+
+      console.log('Your score:', score);
+    };
+
     const queryTerm = 'neon';
     const imgCount = 12;
     fetch(
-      `https://api.unsplash.com/photos/random?client_id=${process.env.REACT_APP_ACCESS_KEY}&count=${imgCount}&query=${queryTerm}`,
+      `https://api.unsplash.com/photos/random?client_id=${process.env.REACT_APP_ACCESS_KEY}&count=${imgCount}&query=${queryTerm}&topics=lights`,
       {
         mode: 'cors',
       }
     )
       .then((response) => response.json())
       .then((data) => {
-        setMemoryCards(
-          initializeMemoryCards(data, props.handleMemoryCardClick)
-        );
+        setMemoryCards(initializeMemoryCards(data, handleMemoryCardClick));
       });
-  }, [props.handleMemoryCardClick]);
+  }, [selectedCards, score]);
 
   return <main>{memoryCards}</main>;
 };
@@ -37,10 +55,7 @@ const initializeMemoryCards = (data, handleClickFn) => {
         handleMemoryCardClick={() => {
           handleClickFn(data[i].urls.small);
         }}
-        // imgSrc={data[i].urls.small}
-        imgSrc={
-          'https://lastfm.freetls.fastly.net/i/u/500x500/df74377533dd4cdad5da59bc675d0156.jpg'
-        }
+        imgSrc={data[i].urls.small}
       />
     );
   }
